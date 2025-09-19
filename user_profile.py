845deaa4e07908500cce1318f4cfa38ebb034f3e
Hingandu <hinganduemmanuel9@gmail.com>
2025-09-19 22:05:40 +0200

    Add user_profile.py

---
+++ user_profile.py
@@ -0,0 +1,12 @@
+# user_profile.py
+
+# Ask user for details
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+language = input("Enter your favorite programming language: ")
+hobby = input("Enter your hobby: ")
+
+# Print formatted summary
+print(f"\nHello {name}!")
+print(f"You are: {age}.")
+print(f"Your favorite programming language is {language} and your hobby is {hobby}.")
